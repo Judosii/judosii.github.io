76c62c3d38906588c310baec240afa3e7e899b65
mskutnik <m.skutnik@rubika-edu.com>
2025-07-02 00:14:44 +0200

    FINALLY THE TEXTURE REPEATS

--- js/earthboundBackground.js
+++ js/earthboundBackground.js
@@ -3,30 +3,40 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
+ctx.imageSmoothingEnabled=false;
+
 let img = new Image();
 img.src = 'js/ebtest.png';
-
-ctx.imageSmoothingEnabled=false;
 
 //set canvas to window size on startup
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let pattern;
+
 img.onload = function(){
-    ctx.fillStyle = ctx.createPattern(img, 'repeat');
-    ctx.drawImage(img, 0,0,canvas.width, canvas.height)
+    pattern = ctx.createPattern(img, 'repeat');
+    Drawbackground()
 }
 
 //prevent stretching on window resize
-window.addEventListener('resize', function(){
+window.addEventListener('resize', ResizeCanvas )
+
+function ResizeCanvas(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    Render()
-})
+    Drawbackground()
+}
 
 function Render(){
-    ctx.createPattern(img, "repeat")
-    ctx.drawImage(img, 0,0,canvas.width, canvas.height)
-    //ctx.fillRect(0,0, canvas.width, canvas.height)
+    ctx.drawImage(img, 0,0,canvas.width, canvas.height);
+    Drawbackground();
+}
+
+function Drawbackground(){
+    if(pattern){
+        ctx.fillStyle = pattern;
+        ctx.fillRect(0,0, canvas.width, canvas.height)
+    }
 }
 
