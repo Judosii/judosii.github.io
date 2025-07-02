0a838ee2030400207bd63c938f05e15635e12d90
mskutnik <m.skutnik@rubika-edu.com>
2025-07-02 16:36:50 +0200

    it work fuck yes

--- js/earthboundBackground.js
+++ js/earthboundBackground.js
@@ -14,6 +14,12 @@ canvas.height = window.innerHeight;
 
 let pattern;
 
+let scrollSpeedX = 0.5; // +right -left
+let scrollSpeedY= 0.5; // +down -up
+let offsetX = 0;
+let offsetY = 0;
+
+
 img.onload = function(){
     pattern = ctx.createPattern(img, 'repeat');
     Drawbackground()
@@ -23,20 +29,30 @@ img.onload = function(){
 window.addEventListener('resize', ResizeCanvas )
 
 function ResizeCanvas(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth + img.width;
+    canvas.height = window.innerHeight + img.height;
     Drawbackground()
-}
-
-function Render(){
-    ctx.drawImage(img, 0,0,canvas.width, canvas.height);
-    Drawbackground();
 }
 
 function Drawbackground(){
     if(pattern){
+        offsetX += scrollSpeedX;
+        offsetY += scrollSpeedY;
+        
+        offsetX %= img.width;
+        offsetY %= img.height;
+        
+        let totalWidth = canvas.width + img.width
+        let totalHeight = canvas.height + img.height
+        
+        ctx.setTransform(1,0,0,1,0,0)
+        ctx.clearRect(0,0,canvas.width, canvas.height)
+        ctx.translate(offsetX, offsetY)
+        
         ctx.fillStyle = pattern;
-        ctx.fillRect(0,0, canvas.width, canvas.height)
+        ctx.fillRect(-img.width,-img.height, totalWidth, totalHeight)
     }
+    requestAnimationFrame(Drawbackground)
+    console.log(offsetX)
 }
 
