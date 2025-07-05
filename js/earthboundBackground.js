692cdd16b36c7344f69a7c26597f5e0c870bb189
mskutnik <m.skutnik@rubika-edu.com>
2025-07-05 18:36:42 +0200

    fixed background speeding up on resize ( i think )

--- js/earthboundBackground.js
+++ js/earthboundBackground.js
@@ -34,7 +34,16 @@ window.addEventListener('resize', ResizeCanvas )
 function ResizeCanvas(){
     canvas.width = window.innerWidth + img.width;
     canvas.height = window.innerHeight + img.height;
-    Drawbackground()
+
+    let totalWidth = canvas.width + img.width
+    let totalHeight = canvas.height + img.height
+    ctx.setTransform(1,0,0,1,0,0)
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    ctx.translate(offsetX, offsetY)
+
+    ctx.fillStyle = pattern;
+    ctx.fillRect(-img.width,-img.height, totalWidth, totalHeight)
+    
 }
 
 function Drawbackground(){
@@ -56,6 +65,4 @@ function Drawbackground(){
         ctx.fillRect(-img.width,-img.height, totalWidth, totalHeight)
     }
     requestAnimationFrame(Drawbackground)
-
-    
 }
