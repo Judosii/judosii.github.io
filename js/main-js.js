83f67b2080e20a41df4ab2f02689e6b042bb29d0
Judosii <skutnik.martin@gmail.com>
2025-08-31 02:00:06 +0200

    darkmode added to the website! press setting button

--- js/main-js.js
+++ js/main-js.js
@@ -1,1 +1,14 @@
-﻿
+﻿function SwitchTheme(button){
+    let darkMode = localStorage.getItem('darkTheme')
+    darkMode !== "active" ? EnableDarkMode(): DisableDarkMode()
+}
+
+function EnableDarkMode(){
+    document.body.classList.add('darkTheme')
+    localStorage.setItem('darkTheme','active')
+}
+
+function DisableDarkMode(){
+    document.body.classList.remove('darkTheme')
+    localStorage.setItem('darkTheme', null)
+}
